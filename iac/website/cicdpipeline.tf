d78314e79ce5ee5dcf43600ac17f97bbc5ea7841
Sunny <sunnym@infopercept.com>
2023-04-21 15:39:01 +0530

    Adding website ci cd IaC

---
+++ iac/website/cicdpipeline.tf
@@ -0,0 +1,112 @@
+#terraform plan for CICD pipeline
+resource "aws_codebuild_project" "tfplan" {
+  name          = "tf-plan2"
+  description   = "Plan stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec.yml")
+ }
+}
+
+#codebuild
+resource "aws_codebuild_project" "tf-applycicd" {
+  name          = "tf-applycicd"
+  description   = "Apply stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.14.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("applybuildspec.yml")
+ }
+}
+
+#codepipeline 
+resource "aws_codepipeline" "tf_cicd_pipeline" {
+
+    name = "terraform-cicd-pipeline"
+    role_arn = aws_iam_role.tf-codepipeline-role.arn
+
+    artifact_store {
+        type="S3"
+        location = aws_s3_bucket.codepipeline-artifacts.id
+    }
+
+    stage {
+        name = "Source"
+        action{
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeStarSourceConnection"
+            version = "1"
+            output_artifacts = ["tf-code"]
+            configuration = {
+                FullRepositoryId = "atcults247/Terraform-CI-CD-Pipeline"
+                BranchName   = "main"
+                ConnectionArn = var.codestar_connector_credentials
+                OutputArtifactFormat = "CODE_ZIP"
+            }
+        }
+    }
+
+    stage {
+        name ="Build"
+        action{
+            name = "Build"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-plan2"
+            }
+        }
+    }
+
+    stage {
+        name ="Deploy"
+        action{
+            name = "Deploy"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-applycicd"
+            }
+        }
+    }
+
+}
---
+++ iac/website/vpc.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc" "DemoVPC" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "DemoV123" {
+  cidr_block = "10.0.0.0/16"
+}
